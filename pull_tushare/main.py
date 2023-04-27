a3ef835c68727edf04eaf2d8bf517c5c1df1bd26
wittfan <wittfan@qq.com>
2023-04-27 15:32:36 +0800

    2023.04.27 23days 完成trade_cal、week、month的自动下载

--- pull_tushare/main.py
+++ pull_tushare/main.py
@@ -5,26 +5,29 @@ import datetime
 def manual_start():
     """ 手动更新所有tushare数据（非定时任务）"""
     pass
-    # 1.年度日历
+    # 1.更新频率：年
+    # 年度日历
     tushare_api_yearly_update = [TradeCalTushare]
     for tushare_api_object in tushare_api_yearly_update:
         tushare_api_object().pull()
 
-    # 2.月行情: 股票月行情、复权行情，指数基本信息、“成分和权重”、申万行业分类、月行情，
+    # 2.更新频率：月
+    # 月行情: 股票月行情、复权行情，指数基本信息、“成分和权重”、申万行业分类、月行情，
     month_list = []
     # 建立一个线程池，遍历month_list
     # 设置pull条件
     # 取下列表的数据日期，上次更新到n月
     # 是否过了n+1月最后一个交易日，如果过了就更新到最新n月
 
-    # 3.周线行情
+    # 3.更新频率：周
+    # 周线行情
     week_list = []
     # 建立一个线程池，遍历week_list
     # 设置pull条件
     # 取下列表的数据日期，上次更新到第n周最后一日
     # 是否过了第n+1周最后一个交易日，如果过了就更新
 
-    # 4.日更
+    # 4.更新频率：日
     day_list = []
     # 更新到今天
 
