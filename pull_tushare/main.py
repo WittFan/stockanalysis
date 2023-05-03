6f2289da1074e9ac2b50f0d5dbed70be34ab86bb
wittfan <wittfanchina@gmail.com>
2023-05-03 23:24:30 +0800

    2023.05.03 27days 制定项目计划

--- pull_tushare/main.py
+++ pull_tushare/main.py
@@ -1,6 +1,7 @@
 import datetime
 from concurrent.futures import ThreadPoolExecutor
 import sys
+
 sys.path.append('.')
 
 from pull_tushare.tushare_tables import *
@@ -8,6 +9,7 @@ from config import cpu_count
 from utils.logger import LoggerTushare
 
 logger = LoggerTushare('pull_tushare_main').logger
+
 
 def manual_start():
     """ 手动更新所有tushare数据（非定时任务）"""
@@ -33,6 +35,7 @@ def manual_start():
     logger.info(' 4.详细信息更新频率：日')
     detail_list = [IndexMemberTushare]  # 月更新
     detail_list += [DaylyTushare, DaylyBasicTushare, AdjFactorTushare]  # 日更新
+
     ## 多线程
     def action(param):
         try:
@@ -40,12 +43,13 @@ def manual_start():
             logger.info(f'线程{param}成功结束')
         except Exception as e:
             logger.error(f'线程{param}遇到错误{e}')
-    with ThreadPoolExecutor(max_workers=cpu_count*2) as pool:
+
+    with ThreadPoolExecutor(max_workers=cpu_count * 2) as pool:
         # with语句会调用executor.shutdown(wait=True)，在所有线程都执行完毕前阻塞当前线程
         # 返回一个生成器，遍历的结果为0,1,2,3。无论执行结果先后顺序如何，看输入的iterator顺序
         # 因为线程池为3，所以0~2进池，其中某个执行完后，3进池
         pool.map(action, detail_list)
 
+
 if __name__ == "__main__":
-    
     manual_start()
